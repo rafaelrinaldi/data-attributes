897f6537dff00b5e8c7f74ca7aa4ef631b69372c
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-11 18:50:54 -0300

    Added camelized keys.

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 (function(exports) {
   'use strict';
 
+  function toUpperCase(string) {
+    return string.toUpperCase();
+  }
+
+  function toLowerCase(string) {
+    return string.toLowerCase();
+  }
+
+  // Shout out to MOUT: https://github.com/mout/mout
+  function camelize(string) {
+    return string
+                .toString()
+                .trim()
+                .replace(/[\-_]/g, ' ')
+                .replace(/\s[a-z]/g, toUpperCase)
+                .replace(/\s+/g, '')
+                .replace(/^[A-Z]/g, toLowerCase);
+  }
+
   function dataAttributes(node) {
     var attributes = {};
     var matchDataAttribute = /^data\-/;
@@ -17,6 +36,8 @@
 
       if(isDataAttribute) {
         name = name.replace(matchDataAttribute, '');
+        name = camelize(name);
+
         attributes[name] = attribute.value;
       }
     }
@@ -31,4 +52,5 @@
   } else {
     exports.dataAttributes = dataAttributes;
   }
+
 })(this);
