c08f20abb28d39149f6314983ec531bffdcf9601
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-11 19:11:01 -0300

    Finished test suite.

--- test.js
+++ test.js
@@ -5,7 +5,16 @@ var test = require('tape');
 var injectMarkup = require('inject-markup');
 var fixture = '<button data-loading-label="Loading..." data-color="#F06">Click me</button>';
 
-injectMarkup(fixture);
+test('data attributes', function(t) {
+  var button = injectMarkup(fixture).children[0];
+  var actual = dataAttributes(button);
+  var expected = {
+    loadingLabel: 'Loading...',
+    color: '#F06'
+  };
 
-console.log(document.body.innerHTML);
+  console.log(JSON.stringify(actual));
 
+  t.deepLooseEqual(actual, expected, 'Expect a literal object with camelized keys');
+  t.end();
+});
